5b4f8026bbd6c64a5e1c3f9de0e6880c61360b6b
Julio Junior <juliojr.datadev@gmail.com>
2024-10-20 22:50:56 -0300

    restaurando repositorio

---
+++ logica de programação 1/parte01/app.js
@@ -0,0 +1,4 @@
+alert('Bem vindo ao jogo do número secreto!');
+let numeroSecreto = 29;
+
+let  = prompt('Escolha um número entre 1 e 30:')
---
+++ logica de programação 1/parte06/app.js
@@ -0,0 +1,27 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p.texto__paragrafo');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute(){
+    //alert('funcionou');
+
+    if(chute == numeroSecreto){
+        
+    }
+
+    let titulo = document.querySelector('h1');
+    titulo.innerHTML = 'Jogo do número secreto';
+
+    let paragrafo = document.querySelector('p.texto__paragrafo');
+    paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+    function verificarChute(){
+        //alert('funcionou');
+
+        if(chute == numeroSecreto){
+            
+        }
+    }
+}
