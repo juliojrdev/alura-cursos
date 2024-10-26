0f71573a5e0175ba5353a64f47dbe40531e71f14
Julio Junior <juliojr.datadev@gmail.com>
2024-10-26 16:30:02 -0300

    atualizando projeto ingressos

--- logica de programação 2/ingresso/js/app.js
+++ logica de programação 2/ingresso/js/app.js
@@ -6,44 +6,46 @@ atualizaQuantidadeDisponivel();
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
     let tipoIngresso = tipo.options[tipo.selectedIndex].value;
-
     let quantidade = parseInt(document.getElementById('qtd').value);
-    console.log(quantidade);
-
-    calculoIngressos(tipoIngresso, quantidade);
-    atualizaQuantidadeDisponivel();
-    document.getElementById('qtd').value = '';
-    tipo.selectedIndex = 0;
-}
-
-function calculoIngressos(tipoIngresso, quantidade) {
-
+    
     if(!tipoIngresso) {
         alert(`[ ! ] Selecione qual o tipo de ingresso que deseja comprar.`); 
     }else if(!quantidade) {
         alert(`[ ! ] Informe a quantidade de ingressos que deseja comprar.`); 
-    }else if(tipoIngresso == 'pista') {
+    }else{
+        calculoIngressos(tipoIngresso, quantidade);
+        atualizaQuantidadeDisponivel();
+        document.getElementById('qtd').value = '';
+        tipo.selectedIndex = 0;
+    }
+}
+
+function calculoIngressos(tipoIngresso, quantidade) {
+    if(tipoIngresso == 'pista') {
         if(quantidade <= qtaPista){
             qtaPista -= quantidade;
+            alert(`[ OK ] Compra realizada com sucesso!`); 
         }else
         alert(`[ ! ] Quantidade de ingresso disponível para Pista é menor do que ${quantidade}`);  
     }else if(tipoIngresso == 'superior') {
         if(quantidade <= qtaSuperior){
             qtaSuperior -= quantidade;
+            alert(`[ OK ] Compra realizada com sucesso!`); 
         }else
         alert(`[ ! ] Quantidade de ingresso disponível para Cadeira Inferior é menor do que ${quantidade}`); 
     }else if(tipoIngresso == 'inferior') {
         if(quantidade <= qtaInferior){
             qtaInferior -= quantidade;
+            alert(`[ OK ] Compra realizada com sucesso!`); 
         }else
         alert(`[ ! ] Quantidade de ingresso disponível para Cadeira Inferior é menor do que ${quantidade}`);
     }
 }
 
 function atualizaQuantidadeDisponivel(){    
-    let quantidadePista = document.getElementById('qtd-pista').textContent = `${qtaPista}`;
-    let quantidadeSuperior = document.getElementById('qtd-superior').textContent = `${qtaSuperior}`;
-    let quantidadeInferior = document.getElementById('qtd-inferior').textContent = `${qtaInferior}`;
+    document.getElementById('qtd-pista').textContent = `${qtaPista}`;
+    document.getElementById('qtd-superior').textContent = `${qtaSuperior}`;
+    document.getElementById('qtd-inferior').textContent = `${qtaInferior}`;
 }
 
 
